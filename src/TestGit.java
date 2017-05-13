52368dc5a42312daddb0b4fa6d4a428c3d8a5323
yaozhichao <785538014@qq.com>
2017-05-13 18:19:48 +0800

    '又增加一行'

--- src/TestGit.java
+++ src/TestGit.java
@@ -4,6 +4,8 @@ public class TestGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("wtf");
+System.out.println("WTF");
+System.out.println("WTF");
 	}
 
 }
